ad27993c997be957ee80baed93ca3898bafcba66
dCben335 <benoit.cabocel.335@gmail.com>
2024-03-20 00:43:17 +0100

    Created light theme and dark one, Start fill the right colors

--- src/components/providers/ThemeContext.tsx
+++ src/components/providers/ThemeContext.tsx
@@ -2,15 +2,23 @@ import { usePathname } from 'next/navigation';
 import React, { createContext, useState, useContext, PropsWithChildren, useEffect, use } from 'react';
 
 const defaultColor = "#F86868";
-type defaultColor = typeof defaultColor;
+type DefaultColor = typeof defaultColor;
+
+const defaultTheme = "light";
+type Theme = "light" | "dark";
+
 interface ThemeContextType {
-    color: string | defaultColor;
+    theme: Theme;
+    color: string | DefaultColor;
     changeColor: (newColor: string) => void;
+    swichTheme: () => void;
 }
 
 const ThemeContext = createContext<ThemeContextType>({
+    theme: defaultTheme,
     color: defaultColor,
     changeColor: () => {},
+    swichTheme: () => {},
 });
 
 export const useTheme = (): ThemeContextType => {
@@ -18,24 +26,35 @@ export const useTheme = (): ThemeContextType => {
 };
 
 export const ThemeProvider = ({ children }: PropsWithChildren<{}>) => {
+    const [theme, setTheme] = useState<Theme>(defaultTheme);
     const [color, setColor] = useState<string>(defaultColor);
-
     const pathname = usePathname()
 
+
     useEffect(() => {
-        document.body.style.setProperty('--accent-color', color);
+        document.documentElement.style.setProperty('--accent-color', color);
     }, [color]);
+    
+    useEffect(() => {
+        document.documentElement.classList.remove(theme === "light" ? "dark" : "light");
+        document.documentElement.classList.add(theme);
+    }, [theme]);
 
     useEffect(() => {
         setColor(defaultColor);
     }, [pathname]);
+    
+
+    const swichTheme = () => {
+        setTheme(theme === "light" ? "dark" : "light");
+    }
 
     const changeColor = (newColor: string) => {
         setColor(newColor);
     };
 
     return (
-        <ThemeContext.Provider value={{ changeColor, color }}>
+        <ThemeContext.Provider value={{ changeColor, color, theme, swichTheme }}>
             {children}
         </ThemeContext.Provider>
     );
