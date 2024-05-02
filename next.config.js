10628916110dd0617394d3ee902b2d4176102b3a
dCben335 <benoit.cabocel.335@gmail.com>
2024-05-02 23:09:39 +0200

    Update next.config

--- next.config.js
+++ next.config.js
@@ -5,6 +5,10 @@ const path = require('path');
 
 
 const nextConfig = {
+    basePath: '/pokedex_front',
+    output: "export",
+    reactStrictMode: true,
+    
     sassOptions: {
         includePaths: [path.join(__dirname, 'styles')],
         prependData: `@import "/src/app/_utils.scss";`
